584f993ceb52049f3cba88b62cbeabf8db7405d2
Nanonumpy <oliphantjt@gmail.com>
2026-02-01 01:02:45 -0700

    GameService Create Game Tests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,6 +1,11 @@
 package service;
 
 import chess.ChessGame;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import model.AuthData;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
 import server.Server;
@@ -8,6 +13,31 @@ import server.Server;
 import java.net.HttpURLConnection;
 import java.util.*;
 
-public class GameServiceTests {
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+public class GameServiceTests {
+    private GameDAO gameDAO;
+    private GameService gameService;
+
+    @BeforeEach
+    public void setup() throws UnauthorizedException {
+        gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        authDAO.createAuth(new AuthData("token1", "user1"));
+        authDAO.createAuth(new AuthData("token2", "user2"));
+        authDAO.createAuth(new AuthData("token3", "user3"));
+        gameService = new GameService(gameDAO, authDAO);
+        assertDoesNotThrow(() ->
+                gameService.createGame("token1", "testGame")
+        );
+    }
+
+    @Test
+    @DisplayName("Create Game With Bad Token")
+    public void generateAuth() {
+        assertThrows(UnauthorizedException.class, () ->
+                gameService.createGame("token4", "badGame")
+        );
+    }
 }
