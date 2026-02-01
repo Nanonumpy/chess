fccb2d04bf976f6a64dd263646e5e5e6632d2884
Nanonumpy <oliphantjt@gmail.com>
2026-02-01 00:51:46 -0700

    Set up Service Unit test classes

---
+++ server/src/test/java/service/AuthServiceTests.java
@@ -0,0 +1,89 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.MemoryAuthDAO;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import passoff.model.TestAuthResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthServiceTests {
+
+    private AuthDAO authDAO;
+    private AuthService authService;
+
+    @BeforeEach
+    public void setup() {
+        authDAO = new MemoryAuthDAO();
+        authDAO.createAuth(new AuthData("token1", "test1"));
+        authDAO.createAuth(new AuthData("token2", "test2"));
+        authService = new AuthService(authDAO);
+    }
+
+    @Test
+    @DisplayName("Generate Auth Successfully")
+    public void generateAuth() {
+        AuthData authData = authService.generateAuth("test3");
+
+
+        Assertions.assertNotNull(authDAO.getAuth(authData.authToken()), "Auth Data not stored");
+    }
+
+    @Test
+    @DisplayName("Generate Existing Auth")
+    public void generateExistingAuth() {
+        AuthData existingAuth = authDAO.getAuth("token2");
+        AuthData authData = authService.generateAuth("test2");
+
+        Assertions.assertNotEquals(existingAuth, authDAO.getAuth(authData.authToken()), "Auth Data not stored");
+    }
+
+    @Test
+    @DisplayName("Validate Existing Auth")
+    public void validateAuth() throws UnauthorizedException {
+        assertDoesNotThrow(() -> {
+            authService.validateAuth("token2");
+        });
+        AuthData authData = authService.validateAuth("token2");
+
+        Assertions.assertEquals("token2", authData.authToken(), "Auth Data not stored");
+    }
+
+    @Test
+    @DisplayName("Validate Auth that doesn't exist")
+    public void validateMissingAuth() {
+        assertThrows(UnauthorizedException.class, () -> {
+            authService.validateAuth("token3");
+        });
+    }
+
+    @Test
+    @DisplayName("Delete Existing Auth")
+    public void deleteAuth() throws UnauthorizedException {
+        assertDoesNotThrow(() -> {
+            authService.deleteAuth(authService.validateAuth("token2"));
+        });
+    }
+
+    @Test
+    @DisplayName("Delete Auth that doesn't exist")
+    public void deleteMissingAuth() {
+        assertDoesNotThrow(() -> {
+            authService.deleteAuth(new AuthData("bad", "bad"));
+        });
+    }
+
+    @Test
+    @DisplayName("clear Auths")
+    public void clear() {
+        assertDoesNotThrow(() -> {
+            authService.clear();
+        });
+        assertNull(authDAO.getAuth("token2"));
+    }
+
+}
---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,13 @@
+package service;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import server.Server;
+
+import java.net.HttpURLConnection;
+import java.util.*;
+
+public class GameServiceTests {
+
+}
