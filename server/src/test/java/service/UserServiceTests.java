c7c2320f2eca4150c27e91b4e4a6ce99a1a67b05
Nanonumpy <oliphantjt@gmail.com>
2026-02-01 01:24:09 -0700

    UserService login tests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,14 +1,10 @@
 package service;
 
-import chess.ChessGame;
 import dataaccess.*;
-import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
-import server.Server;
 
-import java.net.HttpURLConnection;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,6 +32,29 @@ public class UserServiceTests {
     public void registerTaken() {
         assertThrows(AlreadyTakenException.class, () ->
                 userService.register(new UserData("user1", "pass", "email"))
+        );
+    }
+
+    @Test
+    @DisplayName("Login user")
+    public void login() throws UnauthorizedException {
+        LoginResult loginResult = userService.login(new LoginRequest("user1", "pass"));
+        assertEquals("user1", loginResult.username());
+    }
+
+    @Test
+    @DisplayName("Login nonexistent user")
+    public void loginNoUser() {
+        assertThrows(UnauthorizedException.class, () ->
+                userService.login(new LoginRequest("user2", "pass"))
+        );
+    }
+
+    @Test
+    @DisplayName("Login with wrong password")
+    public void loginBadPass() {
+        assertThrows(UnauthorizedException.class, () ->
+                userService.login(new LoginRequest("user1", "idk"))
         );
     }
 }
