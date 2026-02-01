0b1c209be925cb2c201a92911df537ca513ac849
Nanonumpy <oliphantjt@gmail.com>
2026-02-01 01:21:30 -0700

    UserService register tests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,6 +1,9 @@
 package service;
 
 import chess.ChessGame;
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
 import server.Server;
@@ -8,6 +11,31 @@ import server.Server;
 import java.net.HttpURLConnection;
 import java.util.*;
 
-public class UserServiceTests {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class UserServiceTests {
+    private UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+        assertDoesNotThrow(() -> userService.register(new UserData("user1", "pass", "email")));
+    }
+
+    @Test
+    @DisplayName("Register User")
+    public void register() throws AlreadyTakenException {
+        LoginResult loginResult = userService.register(new UserData("user2", "pass", "email"));
+        assertEquals("user2", loginResult.username());
+    }
+
+    @Test
+    @DisplayName("Register Taken User")
+    public void registerTaken() {
+        assertThrows(AlreadyTakenException.class, () ->
+                userService.register(new UserData("user1", "pass", "email"))
+        );
+    }
 }
